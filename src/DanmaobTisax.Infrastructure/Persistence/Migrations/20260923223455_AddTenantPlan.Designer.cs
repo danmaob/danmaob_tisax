89ece099005f21eb4c348193699a9f85bc321888
Luis Ortiz <git@danmaob.com.mx>
2026-09-23 16:38:43 -0600

    Sprint 5: avance 2026-09-23_16:38

--- src/DanmaobTisax.Infrastructure/Persistence/Migrations/20260923223455_AddTenantPlan.Designer.cs
+++ src/DanmaobTisax.Infrastructure/Persistence/Migrations/20260923223455_AddTenantPlan.Designer.cs
@@ -4,6 +4,7 @@ using DanmaobTisax.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DanmaobTisax.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DanmaobTisaxDbContext))]
-    partial class DanmaobTisaxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260923223455_AddTenantPlan")]
+    partial class AddTenantPlan
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/DanmaobTisax.Infrastructure/Persistence/Migrations/20260923223455_AddTenantPlan.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DanmaobTisax.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenantPlan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PlanId",
+                table: "Tenants",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("7a0b3c10-0000-4000-8000-000000000001"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_PlanId",
+                table: "Tenants",
+                column: "PlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tenants_Plans_PlanId",
+                table: "Tenants",
+                column: "PlanId",
+                principalTable: "Plans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tenants_Plans_PlanId",
+                table: "Tenants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tenants_PlanId",
+                table: "Tenants");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "Tenants");
+        }
+    }
+}
