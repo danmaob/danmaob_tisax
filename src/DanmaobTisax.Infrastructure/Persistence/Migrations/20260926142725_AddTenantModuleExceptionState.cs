68eb3cfe960b0c09059c2b7878e43187aef10969
Luis Ortiz <git@danmaob.com.mx>
2026-09-26 08:30:26 -0600

    Sprint 6: US-20-5-03-migration-tenant-module-exception-state 2026-09-26_08:30

---
+++ src/DanmaobTisax.Infrastructure/Persistence/Migrations/20260926142725_AddTenantModuleExceptionState.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DanmaobTisax.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenantModuleExceptionState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEnabled",
+                table: "TenantModules",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TenantModules_ModuleCode",
+                table: "TenantModules",
+                column: "ModuleCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TenantModules_FunctionalModules_ModuleCode",
+                table: "TenantModules",
+                column: "ModuleCode",
+                principalTable: "FunctionalModules",
+                principalColumn: "Code",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TenantModules_FunctionalModules_ModuleCode",
+                table: "TenantModules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TenantModules_ModuleCode",
+                table: "TenantModules");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEnabled",
+                table: "TenantModules",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
